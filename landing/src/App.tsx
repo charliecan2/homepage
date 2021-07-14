2edd4ba48839cfa1abba84b846bf973195533487
ccantu20 <carloscq920@gmail.com>
2021-07-14 11:00:05 -0500

    Initialized create-react-app with typescript and sass installed

---
+++ landing/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <>
+    <h1>Hello World</h1>
+    <h1>This is where the landing page will be!</h1>
+    </>
+  );
+}
+
+export default App;
